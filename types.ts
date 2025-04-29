6008465ac4945bec693c42e9247751a17a0801df
Ryzhikov Aleksandr <ryzhikov.aleksandr.dev@gmail.com>
2025-04-29 13:01:17 +0700

    Define CommandType enum; add Command interface and Program type to represent commands and programs

--- types.ts
+++ types.ts
@@ -13,3 +13,26 @@ export interface Token {
     line: number;
     column: number;
 }
+
+export enum CommandType {
+    PEN = 'PEN',
+    PAPER = 'PAPER',
+    COLOR = 'COLOR',
+    LINE = 'LINE',
+    RECT = 'RECT',
+    CIRCLE = 'CIRCLE',
+    FILL_RECT = 'FILL_RECT',
+    FILL_CIRCLE = 'FILL_CIRCLE',
+    VARIABLE = 'VARIABLE',
+    REPEAT = 'REPEAT',
+    COMMENT = 'COMMENT',
+}
+
+export interface Command {
+    type: CommandType;
+    args: number[];
+    variables?: { [key: string]: number };
+    body?: Command[];
+}
+
+export type Program = Command[];
